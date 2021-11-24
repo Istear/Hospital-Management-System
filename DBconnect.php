9400940649e0b74f0e6749918bf71708b39bb230
Istear <48324430+Istear@users.noreply.github.com>
2021-11-24 18:08:53 +0600

    Add files via upload

---
+++ DBconnect.php
@@ -0,0 +1,12 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="project";
+
+$conn= mysqli_connect($servername,$username,$password,$dbname);
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Hospital Management</title>
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+
+<section id="contact">
+        <h1 class="header">
+            LOG IN
+        </h1>
+            <div class="container">
+             <div class="row">
+             
+             <div class="col-lg-6">
+                <div class="contact-img">
+                 <img src="img/19767-02.png" alt="contact">
+                 </div>
+             </div>
+              <div class="col-lg-6">
+                <div class="form">
+                    <form method="POST" action="Signup.php">
+                       <div>
+                        <input type="text" id="fname" name="fname" required>
+                        <label>Username</label>
+                        </div>
+                        <div>
+                        <input type="password" id="pass" name="pass" required>
+                        <label>Password</label>
+                        </div>
+                        <button type="submit" value="Sign In">Submit</button>
+                    </form>
+                    
+                </div>  
+              </div>
+          </div>
+          </div>
+    </section>
+
+
+
+<script src="js/jquery-1.12.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
